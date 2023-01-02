e6cee5da5e117089ec945ddef4b7f640a69bd133
David Shipley <dave.r.shipley@gmail.com>
2023-01-02 08:47:01 -0700

    Initial Commit

---
+++ Stacker/StackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  StackerApp.swift
+//  Stacker
+//
+//  Created by David Shipley on 1/2/23.
+//
+
+import SwiftUI
+
+@main
+struct StackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
